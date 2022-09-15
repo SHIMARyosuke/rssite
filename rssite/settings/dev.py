c88ac38a1b8ae198e3772c0844de3c38afc11676
rs <shima@unalus.com>
2022-09-16 00:57:01 +0900

    initial commit

---
+++ rssite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-r5s3^cl*e4d*9-)n%#!0xj+(y^tew0%-x^if=wzuj-z5_j%^00"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
